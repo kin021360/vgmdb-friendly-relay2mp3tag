611ac0ea6af9cc75d76d7a0f10486ecd76aed09e
Nathan Lam - desktop <kin021360@yahoo.com.hk>
2020-11-22 14:37:10 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # vgmdb-friendly-relay2mp3tag
+
+The vgmdb data request is forwarded to https://vgmdb.info
+Github: https://github.com/hufman/vgmdb
+
+## Quick Start
+
+Install the dependencies:
+
+```bash
+npm install
+```
+
+Start the Koa server:
+
+```bash
+npm start
+```
+
+mp3tag tagging source script is in development
--- src/routes/vgmdbRelayRouter.js
+++ src/routes/vgmdbRelayRouter.js
@@ -1,7 +1,7 @@
 const Router = require('@koa/router');
 const router = new Router({prefix: '/vgmdb-relay'});
 
-const {handleGetAlbum,handleSearchCatalog} = require('../controllers/vgmdbRelayController');
+const {handleGetAlbum, handleSearchCatalog} = require('../controllers/vgmdbRelayController');
 
 router.get('/album/:id', handleGetAlbum);
 router.get('/search/catalog/:id', handleSearchCatalog);
